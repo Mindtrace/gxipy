c621d7079f18e7325606d83a9a143f407793fd82
pankhuri-mt <122793832+pankhuri-mt@users.noreply.github.com>
2024-07-31 23:40:33 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gxipy",
+    version="2.2.2407.9181",
+    author="Daheng Imaging",
+    description="Python API to use with the Daheng Imaging cameras.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Mindtrace/gxipy",
+    packages=["gxipy"],
+    install_requires=[
+        "numpy"
+        ],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
